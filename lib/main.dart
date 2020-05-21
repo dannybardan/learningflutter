9db031f893e22a634591b0da0f398b04541fe71e
dannybardan <gurungbardan@gmail.com>
2020-05-21 11:01:33 +0800

    initial files

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hello"),
+      ),
+      body: Center(
+        child: Text(
+          "Hello to android",
+          style: TextStyle(
+            fontSize: 20.0,
+            fontFamily: "Preeti",
+          ),
+        ),
+      ),
+    );
+  }
+}
